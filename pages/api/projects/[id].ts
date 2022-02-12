2901d0bac9a9861869ebbcd71cec7515c8272895
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2022-02-12 16:36:15 +0530

    feat: added edit, get by id and delete project api routes

---
+++ pages/api/projects/[id].ts
@@ -0,0 +1,55 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import dbConnect from "../../../lib/mongoose";
+import Project from "../../../models/Project";
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  const {
+    query: { id },
+    method,
+  } = req;
+
+  await dbConnect();
+
+  switch (method) {
+    case "GET":
+      try {
+        const project = await Project.findById(id);
+        if (!project) {
+          return res.status(404).json({ error: "Project not found" });
+        }
+        res.status(200).json({ data: project });
+      } catch (err) {
+        res.status(500).json({ error: err.message });
+      }
+      break;
+    case "PUT":
+      try {
+        const project = await Project.findByIdAndUpdate(id, {
+          name: req.body.name,
+        });
+        if (!project) {
+          return res.status(404).json({ error: "Project not found" });
+        }
+        res.status(200).json({ data: project });
+      } catch (err) {
+        res.status(500).json({ error: err.message });
+      }
+      break;
+    case "DELETE":
+      try {
+        const project = await Project.deleteOne({ _id: id });
+        if (!project) {
+          return res.status(404).json({ error: "Project not found" });
+        }
+        res.status(200).json({ data: project });
+      } catch (err) {
+        res.status(500).json({ error: err.message });
+      }
+      break;
+    default:
+      res.setHeader("Allow", ["GET", "POST"]);
+      res.status(405).end(`Method ${method} Not Allowed`);
+  }
+};
+
+export default handler;
