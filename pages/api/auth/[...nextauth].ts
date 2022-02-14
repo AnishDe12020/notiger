f74951173acb43490ea2df29db2648049d2cab2d
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2022-02-14 16:02:44 +0530

    refactor: remove unused import

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,5 +1,5 @@
 import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
+// import GoogleProvider from "next-auth/providers/google";
 import GitHubProvider from "next-auth/providers/github";
 import { CustomMongooseAdapter } from "../../../lib/CustomMongooseAdapter";
 
