d35dfaff7aed6ef3a0d0d2ac21a110b6dccbde9b
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2022-02-20 15:48:29 +0530

    feat: added stream id validity checks for form and event create api routes

--- pages/api/events/index.ts
+++ pages/api/events/index.ts
@@ -2,6 +2,7 @@ import { ObjectId } from "mongodb";
 import type { NextApiRequest, NextApiResponse } from "next";
 import dbConnect from "../../../lib/dbConnect";
 import Event from "../../../models/Event";
+import Stream from "../../../models/Stream";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const {
@@ -9,41 +10,55 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     query: { streamId },
   } = req;
 
-  await dbConnect();
+  if (!streamId) {
+    return res.status(400).json({
+      error: "Missing streamId",
+    });
+  } else if (!ObjectId.isValid(streamId as string)) {
+    return res.status(400).json({
+      error: "Invalid streamId",
+    });
+  } else if (Stream.exists({ _id: streamId })) {
+    return res.status(400).json({
+      error: "Stream does not exist",
+    });
+  } else {
+    await dbConnect();
 
-  switch (method) {
-    case "GET":
-      try {
-        const events = await Event.find({});
-        res.status(200).json({ data: events });
-      } catch (error) {
-        res.status(400).json({ error: error.message });
-      }
-      break;
+    switch (method) {
+      case "GET":
+        try {
+          const events = await Event.find({});
+          res.status(200).json({ data: events });
+        } catch (error) {
+          res.status(400).json({ error: error.message });
+        }
+        break;
 
-    case "POST":
-      try {
-        const streamIdFormatted = new ObjectId(streamId as string);
-        const event = new Event({
-          streamId: streamIdFormatted,
-          ...req.body,
-        });
+      case "POST":
+        try {
+          const streamIdFormatted = new ObjectId(streamId as string);
+          const event = new Event({
+            streamId: streamIdFormatted,
+            ...req.body,
+          });
 
-        event.save((err, event) => {
-          if (err) {
-            res.status(500).json({ error: err.message });
-          } else {
-            res.status(200).json({ data: event });
-          }
-        });
-      } catch (error) {
-        res.status(400).json({ error: error.message });
-      }
-      break;
+          event.save((err, event) => {
+            if (err) {
+              res.status(500).json({ error: err.message });
+            } else {
+              res.status(200).json({ data: event });
+            }
+          });
+        } catch (error) {
+          res.status(400).json({ error: error.message });
+        }
+        break;
 
-    default:
-      res.setHeader("Allow", ["GET", "POST"]);
-      res.status(405).end(`Method ${method} Not Allowed`);
+      default:
+        res.setHeader("Allow", ["GET", "POST"]);
+        res.status(405).end(`Method ${method} Not Allowed`);
+    }
   }
 };
 
--- pages/api/forms/index.ts
+++ pages/api/forms/index.ts
@@ -2,6 +2,7 @@ import { ObjectId } from "mongodb";
 import type { NextApiRequest, NextApiResponse } from "next";
 import dbConnect from "../../../lib/dbConnect";
 import Form from "../../../models/Form";
+import Stream from "../../../models/Stream";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const {
@@ -9,42 +10,55 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     query: { streamId },
   } = req;
 
-  await dbConnect();
+  if (!streamId) {
+    return res.status(400).json({
+      error: "Missing streamId",
+    });
+  } else if (!ObjectId.isValid(streamId as string)) {
+    return res.status(400).json({
+      error: "Invalid streamId",
+    });
+  } else if (Stream.exists({ _id: streamId })) {
+    return res.status(400).json({
+      error: "Stream does not exist",
+    });
+  } else {
+    await dbConnect();
 
-  switch (method) {
-    case "GET":
-      try {
-        const forms = await Form.find({});
-        res.status(200).json({ data: forms });
-      } catch (error) {
-        res.status(400).json({ error: error.message });
-      }
-      break;
+    switch (method) {
+      case "GET":
+        try {
+          const forms = await Form.find({});
+          res.status(200).json({ data: forms });
+        } catch (error) {
+          res.status(400).json({ error: error.message });
+        }
+        break;
 
-    case "POST":
-      try {
-        console.log(streamId);
-        const streamIdFormatted = new ObjectId(streamId as string);
-        const form = new Form({
-          streamId: streamIdFormatted,
-          ...req.body,
-        });
+      case "POST":
+        try {
+          const streamIdFormatted = new ObjectId(streamId as string);
+          const form = new Form({
+            streamId: streamIdFormatted,
+            ...req.body,
+          });
 
-        form.save((err, form) => {
-          if (err) {
-            res.status(500).json({ error: err.message });
-          } else {
-            res.status(200).json({ data: form });
-          }
-        });
-      } catch (error) {
-        res.status(400).json({ error: error.message });
-      }
-      break;
+          form.save((err, form) => {
+            if (err) {
+              res.status(500).json({ error: err.message });
+            } else {
+              res.status(200).json({ data: form });
+            }
+          });
+        } catch (error) {
+          res.status(400).json({ error: error.message });
+        }
+        break;
 
-    default:
-      res.setHeader("Allow", ["GET", "POST"]);
-      res.status(405).end(`Method ${method} Not Allowed`);
+      default:
+        res.setHeader("Allow", ["GET", "POST"]);
+        res.status(405).end(`Method ${method} Not Allowed`);
+    }
   }
 };
 
