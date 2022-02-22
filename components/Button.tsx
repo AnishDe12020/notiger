37a3cc4d6516ea697339fb912516c6b9d515b40e
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2022-02-22 22:37:31 +0530

    fix(button): added forward ref to forward the ref to the custom button

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,4 +1,4 @@
-import { ReactNode } from "react";
+import { forwardRef, ReactNode } from "react";
 import { Spinner } from "./Icons";
 
 interface IButtonProps {
@@ -10,25 +10,26 @@ interface IButtonProps {
   loading?: boolean;
 }
 
-const Button = ({
-  className,
-  children,
-  danger,
-  onClick,
-  type,
-  loading,
-}: IButtonProps): JSX.Element => {
-  return (
-    <button
-      onClick={onClick}
-      className={`flex items-center justify-center space-x-3 rounded-lg ${
-        danger ? "bg-red-500" : "bg-gray-100"
-      } px-3 py-1 text-lg font-medium text-gray-900 transition duration-200 hover:opacity-60 ${className}`}
-      type={type}
-    >
-      {loading ? <Spinner className="h-7 w-7 text-gray-700" /> : children}
-    </button>
-  );
-};
+const Button = forwardRef<HTMLButtonElement, IButtonProps>(
+  (
+    { className, children, danger, onClick, type, loading }: IButtonProps,
+    ref
+  ): JSX.Element => {
+    return (
+      <button
+        onClick={onClick}
+        className={`flex items-center justify-center space-x-3 rounded-lg ${
+          danger ? "bg-red-500" : "bg-gray-100"
+        } px-3 py-1 text-lg font-medium text-gray-900 transition duration-200 hover:opacity-60 ${className}`}
+        type={type}
+        ref={ref}
+      >
+        {loading ? <Spinner className="h-7 w-7 text-gray-700" /> : children}
+      </button>
+    );
+  }
+);
+
+Button.displayName = "Button";
 
 export default Button;
