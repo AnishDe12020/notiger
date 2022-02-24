fe42693d68685c628c2b803be6db1b771c9ab2f2
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2022-02-24 12:09:56 +0530

    chore(projects-api): secure api routes

--- pages/api/projects/[id].ts
+++ pages/api/projects/[id].ts
@@ -1,59 +1,68 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import dbConnect from "../../../lib/dbConnect";
 import Project from "../../../models/Project";
+import { getToken } from "next-auth/jwt";
+
+const secret = process.env.SECRET;
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  const {
-    query: { id },
-    method,
-  } = req;
+  const token = await getToken({ req, secret });
 
-  await dbConnect();
+  if (token) {
+    const {
+      query: { id },
+      method,
+    } = req;
 
-  switch (method) {
-    case "GET":
-      try {
-        const project = await Project.findById(id);
-        if (!project) {
-          return res.status(404).json({ error: "Project not found" });
+    await dbConnect();
+
+    switch (method) {
+      case "GET":
+        try {
+          const project = await Project.findById(id);
+          if (!project) {
+            return res.status(404).json({ error: "Project not found" });
+          }
+          res.status(200).json(project);
+        } catch (err) {
+          res.status(500).json({ error: err.message });
         }
-        res.status(200).json(project);
-      } catch (err) {
-        res.status(500).json({ error: err.message });
-      }
-      break;
-    case "PUT":
-      try {
-        const project = await Project.findByIdAndUpdate(
-          id,
-          {
-            name: req.body.name,
-            description: req.body.description,
-          },
-          { new: true }
-        );
-        if (!project) {
-          return res.status(404).json({ error: "Project not found" });
+        break;
+      case "PUT":
+        try {
+          const project = await Project.findByIdAndUpdate(
+            id,
+            {
+              name: req.body.name,
+              description: req.body.description,
+            },
+            { new: true }
+          );
+          if (!project) {
+            return res.status(404).json({ error: "Project not found" });
+          }
+          res.status(200).json(project);
+        } catch (err) {
+          res.status(500).json({ error: err.message });
         }
-        res.status(200).json(project);
-      } catch (err) {
-        res.status(500).json({ error: err.message });
-      }
-      break;
-    case "DELETE":
-      try {
-        const project = await Project.deleteOne({ _id: id });
-        if (!project) {
-          return res.status(404).json({ error: "Project not found" });
+        break;
+      case "DELETE":
+        try {
+          const project = await Project.deleteOne({ _id: id });
+          if (!project) {
+            return res.status(404).json({ error: "Project not found" });
+          }
+          res.status(200).json(project);
+        } catch (err) {
+          res.status(500).json({ error: err.message });
         }
-        res.status(200).json(project);
-      } catch (err) {
-        res.status(500).json({ error: err.message });
-      }
-      break;
-    default:
-      res.setHeader("Allow", ["GET", "POST"]);
-      res.status(405).end(`Method ${method} Not Allowed`);
+        break;
+      default:
+        res.setHeader("Allow", ["GET", "POST"]);
+        res.status(405).end(`Method ${method} Not Allowed`);
+    }
+  } else {
+    return res.status(401).json({ error: "Unauthorized" });
   }
 };
 
--- pages/api/projects/index.ts
+++ pages/api/projects/index.ts
@@ -2,48 +2,57 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import { ObjectId } from "mongodb";
 import dbConnect from "../../../lib/dbConnect";
 import Project from "../../../models/Project";
+import { getToken } from "next-auth/jwt";
+
+const secret = process.env.SECRET;
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  const { method } = req;
+  const token = await getToken({ req, secret });
 
-  await dbConnect();
+  if (token) {
+    const { method } = req;
 
-  switch (method) {
-    case "GET":
-      try {
-        const ownerId = req.query.ownerId as string;
-        const projects = await Project.find({ ownerId: ownerId });
-        res.status(200).json(projects);
-      } catch (err) {
-        res.status(400).json({ error: err.message });
-      }
-      break;
-    case "POST":
-      try {
-        console.log(req.body);
-        const ownerId = new ObjectId(req.body.ownerId);
-        const project = new Project({
-          name: req.body.name,
-          description: req.body?.description,
-          ownerId: ownerId,
-        });
+    await dbConnect();
 
-        project.save((err, project) => {
-          if (err) {
-            console.log(err);
-            return res.status(400).json({ error: err.message });
-          } else {
-            return res.status(200).json(project);
-          }
-        });
-      } catch (err) {
-        return res.status(400).json({ error: err.message });
-      }
-      break;
+    switch (method) {
+      case "GET":
+        try {
+          const ownerId = req.query.ownerId as string;
+          const projects = await Project.find({ ownerId: ownerId });
+          res.status(200).json(projects);
+        } catch (err) {
+          res.status(400).json({ error: err.message });
+        }
+        break;
+      case "POST":
+        try {
+          console.log(req.body);
+          const ownerId = new ObjectId(req.body.ownerId);
+          const project = new Project({
+            name: req.body.name,
+            description: req.body?.description,
+            ownerId: ownerId,
+          });
 
-    default:
-      res.setHeader("Allow", ["GET", "POST"]);
-      return res.status(405).end(`Method ${method} Not Allowed`);
+          project.save((err, project) => {
+            if (err) {
+              console.log(err);
+              return res.status(400).json({ error: err.message });
+            } else {
+              return res.status(200).json(project);
+            }
+          });
+        } catch (err) {
+          return res.status(400).json({ error: err.message });
+        }
+        break;
+
+      default:
+        res.setHeader("Allow", ["GET", "POST"]);
+        return res.status(405).end(`Method ${method} Not Allowed`);
+    }
+  } else {
+    return res.status(401).json({ error: "Unauthorized" });
   }
 };
 
