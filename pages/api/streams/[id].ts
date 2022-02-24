0506394d88b40978a7e336d3557b77fa4a06fd40
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2022-02-24 12:13:01 +0530

    chore(streams-api): secure api routes

--- pages/api/streams/[id].ts
+++ pages/api/streams/[id].ts
@@ -1,68 +1,77 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import dbConnect from "../../../lib/dbConnect";
 import Stream from "../../../models/Stream";
+import { getToken } from "next-auth/jwt";
+
+const secret = process.env.SECRET;
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  const {
-    query: { id },
-    method,
-  } = req;
+  const token = await getToken({ req, secret });
 
-  await dbConnect();
+  if (token) {
+    const {
+      query: { id },
+      method,
+    } = req;
 
-  switch (method) {
-    case "GET":
-      try {
-        const stream = await Stream.findById(id);
-        if (!stream) {
-          return res.status(404).json({
-            error: "Stream not found",
-          });
+    await dbConnect();
+
+    switch (method) {
+      case "GET":
+        try {
+          const stream = await Stream.findById(id);
+          if (!stream) {
+            return res.status(404).json({
+              error: "Stream not found",
+            });
+          }
+          res.status(200).json(stream);
+        } catch (err) {
+          res.status(500).json({ error: err.message });
         }
-        res.status(200).json(stream);
-      } catch (err) {
-        res.status(500).json({ error: err.message });
-      }
-      break;
+        break;
 
-    case "PUT":
-      try {
-        const stream = await Stream.findByIdAndUpdate(
-          id,
-          {
-            name: req.body.name,
-            description: req.body.description,
-          },
-          { new: true }
-        );
-        if (!stream) {
-          return res.status(404).json({
-            error: "Stream not found",
-          });
+      case "PUT":
+        try {
+          const stream = await Stream.findByIdAndUpdate(
+            id,
+            {
+              name: req.body.name,
+              description: req.body.description,
+            },
+            { new: true }
+          );
+          if (!stream) {
+            return res.status(404).json({
+              error: "Stream not found",
+            });
+          }
+          res.status(200).json(stream);
+        } catch (err) {
+          res.status(500).json({ error: err.message });
         }
-        res.status(200).json(stream);
-      } catch (err) {
-        res.status(500).json({ error: err.message });
-      }
-      break;
+        break;
 
-    case "DELETE":
-      try {
-        const stream = await Stream.deleteOne({ _id: id });
-        if (!stream) {
-          return res.status(404).json({
-            error: "Stream not found",
-          });
+      case "DELETE":
+        try {
+          const stream = await Stream.deleteOne({ _id: id });
+          if (!stream) {
+            return res.status(404).json({
+              error: "Stream not found",
+            });
+          }
+          res.status(200).json(stream);
+        } catch (err) {
+          res.status(500).json({ error: err.message });
         }
-        res.status(200).json(stream);
-      } catch (err) {
-        res.status(500).json({ error: err.message });
-      }
-      break;
+        break;
 
-    default:
-      res.setHeader("Allow", ["GET", "PUT", "DELETE"]);
-      res.status(405).end(`Method ${method} not allowed`);
+      default:
+        res.setHeader("Allow", ["GET", "PUT", "DELETE"]);
+        res.status(405).end(`Method ${method} not allowed`);
+    }
+  } else {
+    return res.status(401).json({ error: "Unauthorized" });
   }
 };
 
--- pages/api/streams/index.ts
+++ pages/api/streams/index.ts
@@ -2,48 +2,57 @@ import { ObjectId } from "mongodb";
 import type { NextApiRequest, NextApiResponse } from "next";
 import dbConnect from "../../../lib/dbConnect";
 import Stream from "../../../models/Stream";
+import { getToken } from "next-auth/jwt";
+
+const secret = process.env.SECRET;
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  const { method } = req;
+  const token = await getToken({ req, secret });
 
-  await dbConnect();
+  if (token) {
+    const { method } = req;
 
-  switch (method) {
-    case "GET":
-      try {
-        const projectId = req.query.projectId as string;
-        const streams = await Stream.find({ projectId: projectId });
-        res.status(200).json(streams);
-      } catch (error) {
-        res.status(400).json({ error: error.message });
-      }
-      break;
+    await dbConnect();
 
-    case "POST":
-      try {
-        console.log(req.body);
-        const projectId = new ObjectId(req.body.projectId);
-        const stream = new Stream({
-          name: req.body.name,
-          description: req.body?.description,
-          projectId: projectId,
-        });
+    switch (method) {
+      case "GET":
+        try {
+          const projectId = req.query.projectId as string;
+          const streams = await Stream.find({ projectId: projectId });
+          res.status(200).json(streams);
+        } catch (error) {
+          res.status(400).json({ error: error.message });
+        }
+        break;
 
-        stream.save((err, stream) => {
-          if (err) {
-            res.status(500).json({ error: err.message });
-          } else {
-            res.status(200).json(stream);
-          }
-        });
-      } catch (error) {
-        res.status(400).json({ error: error.message });
-      }
-      break;
+      case "POST":
+        try {
+          console.log(req.body);
+          const projectId = new ObjectId(req.body.projectId);
+          const stream = new Stream({
+            name: req.body.name,
+            description: req.body?.description,
+            projectId: projectId,
+          });
 
-    default:
-      res.setHeader("Allow", ["GET", "POST"]);
-      res.status(405).end(`Method ${method} Not Allowed`);
+          stream.save((err, stream) => {
+            if (err) {
+              res.status(500).json({ error: err.message });
+            } else {
+              res.status(200).json(stream);
+            }
+          });
+        } catch (error) {
+          res.status(400).json({ error: error.message });
+        }
+        break;
+
+      default:
+        res.setHeader("Allow", ["GET", "POST"]);
+        res.status(405).end(`Method ${method} Not Allowed`);
+    }
+  } else {
+    return res.status(401).json({ error: "Unauthorized" });
   }
 };
 
