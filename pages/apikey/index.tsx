c727c60c32fe77fe1b1abda4bc24e3c5fd1795d5
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2022-02-25 12:46:26 +0530

    chore(api-keys): add local state mutation when new api key is generated

--- pages/apikey/index.tsx
+++ pages/apikey/index.tsx
@@ -1,12 +1,13 @@
 import axios from "axios";
 import { NextPage } from "next";
 import toast from "react-hot-toast";
-import useSWR from "swr";
+import useSWR, { useSWRConfig } from "swr";
 import ApiKey from "../../components/ApiKey";
 import Button from "../../components/Button";
 
 const ApiKeyPage: NextPage = () => {
   const { data: apiKeys, error } = useSWR("/api/apiKey");
+  const { mutate } = useSWRConfig();
 
   if (error) {
     toast.error("Something went wrong!");
@@ -17,10 +18,11 @@ const ApiKeyPage: NextPage = () => {
   const generateApiKey = async () => {
     const res = await axios.post("/api/apiKey");
     console.log(res);
+    mutate("/api/apiKey");
   };
 
   return (
-    <div className="mx-8 mt-32 flex flex-col space-y-16">
+    <div className="mx-8 mt-32 flex flex-col space-y-16 md:mx-16 lg:mx-32">
       <Button onClick={generateApiKey} className="md: w-fit">
         Generate API Key
       </Button>
