a6c3c955a1c8c9ced097a7071b72c94539472f66
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2022-02-28 12:28:13 +0530

    chore: add logo to header

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,11 +1,15 @@
 import NavLink from "./NavLink";
 import { useRouter } from "next/router";
+import { Logo } from "../Icons";
 
 const Header = (): JSX.Element => {
   const router = useRouter();
 
   return (
-    <nav className="mx-8 mt-4 flex items-center justify-end md:mx-16">
+    <nav className="mx-8 mt-4 flex items-center justify-between md:mx-16">
+      <NavLink currentRoute={router.pathname} href="/">
+        <Logo className="h-16 w-16" />
+      </NavLink>
       <div className="flex space-x-4">
         <NavLink currentRoute={router.pathname} href="/dashboard">
           Dashboard
